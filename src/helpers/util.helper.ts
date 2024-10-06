23d5e485965f399d1ee799f6b628ccd394be2b77
Dafcoe <dafcoe@gmail.com>
2024-10-06 18:45:10 +0100

    Improves processing output log

--- src/helpers/util.helper.ts
+++ src/helpers/util.helper.ts
@@ -1,3 +1,10 @@
+interface CursorPositionInterface {
+  row: number;
+  column: number;
+}
+
+const cliOutput: Record<string, string> = {};
+
 export function wait(timeInMs: number = 1000) {
   return new Promise(resolve => setTimeout(resolve, timeInMs));
 }
@@ -5,16 +12,53 @@ export function wait(timeInMs: number = 1000) {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function logExecutionTime<T extends (...args: any[]) => Promise<any>>(fn: T, description?: string): T {
   return async function (...args: Parameters<T>): Promise<ReturnType<T>> {
-    process.stdout.write(`⚙️${description || `Running ${fn.name}`} ... `);
+    let cursorPosition: CursorPositionInterface = { row : 0, column: 0 };
+    const output = `⚙️ ${description || `Running ${fn.name}`} ... `;
+
+    cliOutput[output] = output;
+    console.log(output);
+
+    await getCursorPosition((position: CursorPositionInterface) => {
+      cursorPosition = position;
+    });
 
     const startTime = performance.now();
     const fnResult = await fn(...args);
     const endTime = performance.now();
 
-    process.stdout.write(`done (⏱️${(endTime - startTime).toFixed(2)} ms)\n`);
+    cliOutput[output] = `${cliOutput[output]}done [⏱️ ${(endTime - startTime).toFixed(2)} ms]`;
+
+    const cliOutputLength = Object.keys(cliOutput).length;
+
+    process.stdout.write(`\x1b[${cursorPosition.row - cliOutputLength};${cursorPosition.column}H`);
+    Object.values(cliOutput).forEach((cliOutputEntry) => { console.log(cliOutputEntry); });
 
     return fnResult;
   } as T;
+}
+
+function getCursorPosition(callback: CallableFunction): Promise<boolean> {
+  return new Promise((resolve) => {
+    process.stdin.resume();
+    process.stdin.setRawMode(true);
+
+    process.stdin.once('data', (buffer) => {
+      const  match = /\[(\d+);(\d+)R$/.exec(buffer.toString());
+
+      if (match) {
+        const position = match.slice(1, 3).reverse().map(Number);
+
+        callback({ row: position[1], column: position[0] });
+        resolve(true);
+      }
+
+      process.stdin.setRawMode(false);
+      process.stdin.pause();
+    });
+
+    process.stdout.write('\x1b[6n');
+    process.stdout.emit('data', '\x1b[6n');
+  });
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
