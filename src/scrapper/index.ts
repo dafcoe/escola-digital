dea42434673223ec1206359e89018ecccf19906f
Dafcoe <dafcoe@gmail.com>
2025-10-02 20:24:01 +0100

    Rollback to simpler scrapper functionality

--- src/scrapper/index.ts
+++ src/scrapper/index.ts
@@ -8,45 +8,21 @@ async function run(): Promise<void> {
   const browser = await puppeteer.launch({
     headless: true,
     executablePath,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    timeout: 600000,
   });
   globalThis.page  = await browser.newPage();
   await globalThis.page.setViewport({ width: 1920, height: 1080 });
-  const cliArguments = getCliArguments();
 
   await login();
-  await generateStudentReports(cliArguments);
+  await generateStudentReports();
   await logout();
 
   await browser.close();
 }
 
-function getCliArguments()  {
-  const args: Record<string, string> = {};
-
-  process.argv.slice(2).forEach((arg) => {
-    const [key, value] = arg.split('=');
-
-    args[key.replace('--', '')] = value;
-  });
-
-  if (!args.name || !args.nifs) {
-    throw new Error('"name" or "nifs" argument is missing');
-  }
-
-  return {
-    [args.name || 'report-name']: (args.nifs || '')
-      .replaceAll(' ', '')
-      .split(',')
-      .filter((nif) => nif.length > 0),
-  };
-};
-
-async function generateStudentReports(cliArguments: Record<string, string[]> = {}): Promise<void> {
-  const studentNifsByClassName: Record<string, string[]>[] = [
-    cliArguments,
-    // { 'report-name': ['123456789'] },
-  ];
+async function generateStudentReports(): Promise<void> {
+  const studentNifsByClassName: Record<string, string[]>[] = [];
 
   for (let index = 0; index < studentNifsByClassName.length; index++) {
     const nifsByClassName = Object.values(studentNifsByClassName)[index];
